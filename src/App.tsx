66cd80bdbbdb4e7901b60380a2a3d5b23330c8b5
Charles Xu <char8x@gmail.com>
2023-03-02 11:06:15 +0800

    github pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { Input, Button } from "antd";
 import _debounce from 'lodash/debounce'
 import { drawBinaryTree } from 'binary-tree-visualizer';
-import BinaryTree from './binaryTree.js'
+import BinaryTree from './binaryTree'
 
 import styles from './App.module.css'
 import './App.css'
@@ -14,7 +14,7 @@ function App() {
   const handleTextChange = (e) => setText(e.target.value)
 
   const handleEnter = () => {
-    const arr = text.split(',')
+    const arr = text.replaceAll('[', '').replaceAll(']', '').replaceAll(/\s/g, '').split(',')
     if (arr.some(v => v !== "")) {
       const tree = BinaryTree.from(arr)
       // @ts-ignore
